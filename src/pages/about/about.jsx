1e3f823c80f943e54e4e8553141491cfdbb95c48
chi-touch <gloebudav@gmail.com>
2025-01-30 16:56:05 +0100

    fixing the portfolio

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./index.module.css";
-// import CV from "../../../../my-portfolio/src/assets/Glory Cv.pdf"
+
 import CV from "../../assets/Glory Cv.pdf"
 import Info from "./info";
 
